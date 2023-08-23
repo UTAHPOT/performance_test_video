fa16c2ef55911a03381d0057a8be7e35cc85552e
Tim Levytskyi <tlevickij@gmail.com>
2023-08-23 13:09:20 +0300

    First commit

---
+++ performance-canvas/script.js
@@ -0,0 +1,20 @@
+let mainDiv = document.querySelector('#main-div');
+const width  = ((window.innerWidth / 4) / window.innerWidth) * 100 + 'vw';
+const height = ((window.innerHeight / 5) / window.innerHeight) * 100 + 'vh';
+
+for (let i = 0; i < 5; i++) {
+    let childDiv = document.createElement('div');
+    childDiv.classList.add('child-div');
+    childDiv.setAttribute('style', `height: ${height}; width: ${window.innerWidth};`);
+    mainDiv.append(childDiv);
+    for(let x = 0; x < 4; x++) {
+        let canvasSquare = document.createElement('video');
+        canvasSquare.classList.add('canvas-square');
+        canvasSquare.setAttribute('style', `height: ${height}; width: ${width};`);
+        canvasSquare.setAttribute("src", "video_2.mp4");
+        canvasSquare.muted = true;
+        canvasSquare.autoplay = true;
+        canvasSquare.loop = true;
+        childDiv.append(canvasSquare);
+    }
+}
